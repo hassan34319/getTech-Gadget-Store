73a6200dc6078e04372c1530018ed45926e81f57
hassan34319 <mh08062@st.habib.edu.pk>
2023-03-19 02:52:03 +0500

    New

--- components/About/contact.tsx
+++ components/About/contact.tsx
@@ -7,12 +7,12 @@ const Contact5 = () => {
   const emailRef = useRef();
   const messageRef = useRef();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    /* 
-      Do something here !
-      */
-  };
+  // const handleSubmit = (e : ) => {
+  //   e.preventDefault();
+  //   /* 
+  //     Do something here !
+  //     */
+  // };
 
   return (
     <div className="bg-tra relative mx-auto w-full max-w-7xl space-y-5">
@@ -27,7 +27,6 @@ const Contact5 = () => {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8827.330741966553!2d2.308756110118289!3d48.87000842543867!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66fc4f8f3049b%3A0xcbb47407434935db!2s18%20Av.%20des%20Champs-%C3%89lys%C3%A9es%2C%2075008%20Paris!5e0!3m2!1sfr!2sfr!4v1635492407441!5m2!1sfr!2sfr"
             title="map"
             scrolling="no"
-            frameborder="0"
             width="100%"
             height="300px"
             className=""
@@ -40,7 +39,7 @@ const Contact5 = () => {
           <form
             action=""
             className="mx-auto max-w-xl space-y-4"
-            onSubmit={handleSubmit}
+            // onSubmit={handleSubmit}
           >
             {/* ::Name Input */}
             <div>
@@ -50,7 +49,7 @@ const Contact5 = () => {
               </label>
               {/* :::input */}
               <input
-                ref={nameRef}
+                // ref={nameRef}
                 type="text"
                 id="name"
                 name="name"
@@ -66,7 +65,7 @@ const Contact5 = () => {
               </label>
               {/* :::input */}
               <input
-                ref={emailRef}
+                // ref={emailRef}
                 type="email"
                 id="email"
                 name="email"
@@ -82,7 +81,7 @@ const Contact5 = () => {
               </label>
               {/* :::input */}
               <textarea
-                ref={messageRef}
+                // ref={messageRef}
                 name="message"
                 id="message"
                 cols={30}
