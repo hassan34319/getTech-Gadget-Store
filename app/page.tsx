56ef7b09b68a5d38fa98ce63fe97945ce4e8516a
hassan34319 <mh08062@st.habib.edu.pk>
2023-03-19 23:53:09 +0500

    newerrrr

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,33 @@
-
 import Landing from "../components/Landing";
-import NewPromos from "../components/HomePageProducts"
+import NewPromos from "../components/HomePageProducts";
 import { groq } from "next-sanity";
 import { sanityClient } from "../sanity";
 
-const Home = async() => {
-    const getProducts = async () => {
-        const res = await fetch(
-          `${process.env.NEXT_PUBLIC_BASE_URL}/api/getProducts`, { cache: 'no-store' }, //Sends request to your own backend API
-        );
-      
-        const data = await res.json()
-        const products : Product[] = data.products
-      
-        return products
-      };
+const Home = async () => {
+  const getProducts = async () => {
+    const query = groq`*[_type == "product"] {
+        _id,
+          ...
+      } | order(_createdAt desc)`;
+    const products = await sanityClient.fetch(query);
 
-      const getCategories = async () => {
-        const query = groq`*[_type == "category"] { _id,... }`;
-        const categories =  await sanityClient.fetch(query);
-    
-        return categories;
-      };
-      
-      const products_promise = getProducts()
-      const categories_promise = getCategories()
+    return products;
+  };
 
-      const [products, categories] = await Promise.all([products_promise, categories_promise]);
+  const getCategories = async () => {
+    const query = groq`*[_type == "category"] { _id,... }`;
+    const categories = await sanityClient.fetch(query);
+
+    return categories;
+  };
+
+  const products_promise = getProducts();
+  const categories_promise = getCategories();
+
+  const [products, categories] = await Promise.all([
+    products_promise,
+    categories_promise,
+  ]);
 
   return (
     <div className="">
@@ -45,16 +46,18 @@ const Home = async() => {
         {/* Screen size is 100vh, 200vh means that content of same size as the screen lies under it I.e when we scroll. This is done so that our content is scrollable and so that we can do our animation of bring the new promos up */}
         <Landing />
       </main>
-      <section id="section-1" className="relative z-40 -mt-[100vh] min-h-screen bg-[#28282B] ">
+      <section
+        id="section-1"
+        className="relative z-40 -mt-[100vh] min-h-screen bg-[#28282B] "
+      >
         {/* {-mt-[100-vh] allows it to come on top as we scroll down because we have -100 margin from top} */}
-        <NewPromos categories={categories} products={products}/>
+        <NewPromos categories={categories} products={products} />
       </section>
     </div>
   );
 };
 
 export default Home;
-
 
 // export const getServerSideProps: GetServerSideProps<QueryProps> = async () => {
 //   // This means getserverSideProps is of type GetServerSideProps
