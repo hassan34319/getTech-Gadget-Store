4928248c4c1eab9a7de9889e54a4914eacfbde7a
hassan34319 <mh08062@st.habib.edu.pk>
2023-03-20 23:56:50 +0500

    node 16

--- app/success/layout.tsx
+++ app/success/layout.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-function layout() {
+function layout( {
+    // Layouts must accept a children prop.
+    // This will be populated with nested layouts or pages
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
   return (
-    <div>layout</div>
+    <div>{children}</div>
   )
-}
+  }
 
 export default layout
